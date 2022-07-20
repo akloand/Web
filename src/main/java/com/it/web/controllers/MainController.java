4b42116b65afaa7160a177d069f6928696c73d09
akloand <akloand@gmail.com>
2022-07-20 19:31:36 +0200

    20.07.2022

---
+++ src/main/java/com/it/web/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.it.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страННица");
+        return "home";
+    }
+
+}
